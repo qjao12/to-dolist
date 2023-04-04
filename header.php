f878b9db62930e9004fb792d4d4c8c9cf82558e7
qjao12 <qjaobias@gmail.com>
2023-04-04 13:58:11 +0800

    added CDN from index

--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body> 
+
 <nav class="navbar navbar-expand-lg bg-light navbar-light fixed-top">
       <div class="container">
         <a href="index.php" class="navbar-brand">To-Do List</a>
@@ -12,7 +26,7 @@
         <div class="collapse navbar-collapse" id="navmenu">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a href="#" class="nav-link">Sign In</a>
+              <a href="login.php" class="nav-link">Sign In</a>
             </li>
             <li class="nav-item">
               <a href="#" class="nav-link">Sign Up</a>
