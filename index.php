5c3ff2635dc0427f107b7e7739e44fa850a9425f
perezyaneh <perezyaneh00@gmail.com>
2023-04-04 13:12:39 +0800

    new changes ui

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,17 +9,28 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <?php 
+    <?php
     include 'function.php';
     ?>
-    <form action="index.php" method="POST">
-        Task: <input type="text" name="task">
-        <input type="submit">
-    </form>
-    <?php 
+
+    <section class="bg-dark p-5">
+        <form class="row g-3">
+            <div class="input-group mb-3">
+                <span class="input-group-text" id="basic-addon1">Task</span>
+                <input type="text" name="task" class="form-control" placeholder="Enter Task" aria-label="Username" aria-describedby="basic-addon1">
+                <button class="btn btn-primary" type="button">Submit</button>
+            </div>
+        </form>
+        <?php
         $task = $_POST['task'];
         echo $task;
-    ?>
+        ?>
+        </div>
+        </div>
+    </section>
+
 </body>
+
 </html>
