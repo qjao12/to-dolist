246adffed9686c12528c4e6b6d983afa7ebc380b
perezyaneh <perezyaneh00@gmail.com>
2023-04-04 11:28:19 +0800

    changes

--- index.php
+++ index.php
@@ -5,10 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To Do List</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php 
     include 'function.php';
+    ?>
+    <form action="index.php" method="POST">
+        Task: <input type="text" name="task">
+        <input type="submit">
+    </form>
+    <?php 
+        $task = $_POST['task'];
+        echo $task;
     ?>
 </body>
 </html>
