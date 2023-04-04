68f307bc517bf91452daa886d268eeb849863a16
perezyaneh <perezyaneh00@gmail.com>
2023-04-04 16:55:53 +0800

    new version

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
+<?php include 'header.php' ?>
+<section class="bg-dark p-5 text-dark">
+    <form action="index.php" method="POST">
+        <div class="input-group mb-3 mt-5">
+            <span class="input-group-text" id="basic-addon1">Task:</span>
+            <input type="text" name="sample[]" class="form-control" placeholder="Enter Task">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+</section>
+<?php
 
-    <?php include 'header.php' ?>
-    <section class="bg-dark p-5 text-dark">
-        <form action="index.php" method="POST">
-            <div class="input-group mb-3 mt-5">
-                <span class="input-group-text" id="basic-addon1">Task:</span>
-                <input type="text" name="sample" class="form-control" placeholder="Enter Task">
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </div>
-        </form>
-    </section>
-    <?php
-    $task = $_POST['sample'];
-    echo $task;
-    ?>
+
+if (isset($_POST['sample']) and is_array($_POST['sample'])) {
+    foreach ($_POST['sample'] as $task) {
+        print $task;
+    }
+} else {
+}
+
+?>
 </body>
 
 </html>
