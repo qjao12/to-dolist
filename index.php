71be5c090c6e4a5972894db4e9fcd66b85bd2728
perezyaneh <perezyaneh00@gmail.com>
2023-04-05 09:04:07 +0800

    v2 changes

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php include 'header.php' ?>
+<?php session_start() ?>
 <section class="bg-dark p-5 text-dark">
     <form action="index.php" method="POST">
         <div class="input-group mb-3 mt-5">
             <span class="input-group-text" id="basic-addon1">Task:</span>
-            <input type="text" name="sample[]" class="form-control" placeholder="Enter Task">
+            <input type="text" name="sample" class="form-control" placeholder="Enter Task">
             <button type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
@@ -11,12 +12,19 @@
 <?php
 
 
-if (isset($_POST['sample']) and is_array($_POST['sample'])) {
-    foreach ($_POST['sample'] as $task) {
-        print $task;
-    }
-} else {
+
+$myfile = fopen("newfile.txt", "r") or die("Unable to open file!");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+    $newTask = $_POST['sample'];
+    fwrite($myfile, "$newTask \n");
 }
+
+
+echo file_get_contents("newfile.txt") . "<br>";
+
+
 
 ?>
 </body>
